1a748372c2671af3357d81c900f8e9d420dcc8e0
priscillaarslan <parslan@udesa.edu.ar>
2022-09-12 11:41:12 -0300

    agregue

--- MyApp/controllers/PostController.js
+++ MyApp/controllers/PostController.js
@@ -1,9 +1,21 @@
 let datapost = require('../data/posteos')
+
 const PostController = {
+
+    agregar: function (req,res,next) {
+        res.render ('agregarPost')
+    },
+
     index: function (req,res,next) {
         res.render ('index',{
-            data: datapost
+            data: datapost,
+            title: 'Home',
+            usuario: 'jperez'
         })
+    },
+
+    detalle: function (req,res,next) {
+        res.render ('detallePost')
     }
 }
 module.exports = PostController
--- MyApp/controllers/UserController.js
+++ MyApp/controllers/UserController.js
@@ -1,9 +1,49 @@
-let datapost = require('../data/posteos')
-const PostController = {
-    index: function (req,res,next) {
-        res.render ('index',{
-            data: datapost
+const db = require('../database/index');
+
+const UserController = {
+    login: function (req,res) {
+        res.render ('login')
+    },
+
+    autenticar: function (req,res) {
+        const usuario = req.body.usuario;
+        const contrasena = req.body.contrasena;
+    },
+
+    registracion: function (req,res) {
+        res.render ('registracion')
+    },
+
+    registrar: function (req,res) {
+        db.User.create({
+            usuario: req.body.usuario,
+            email: req.body.email,
+            contrasena: req.body.contrasena,
+            dni: req.body.dni,
+            foto: 'ss'
         })
+        .then(function(){
+            res.redirect('/')
+        })
+        .catch(function(error){
+            console.log(error);            
+        }) 
+    },
+
+    perfil: function (req,res) {
+        res.render ('miPerfil')
+    },
+
+    editarPerfil: function (req,res) {
+        res.render ('editarPerfil')
+    },
+
+    detalle: function (req,res) {
+        res.render ('detalleUsuario')
+    },
+
+    buscar: function (req,res) {
+        res.render ('resultadoBusqueda')
     }
 }
-module.exports = PostController
+module.exports = UserController
