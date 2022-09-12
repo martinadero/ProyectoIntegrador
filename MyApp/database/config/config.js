1110e59e29c1e4cdf89471fcc0656b1f6cd6c0f5
priscillaarslan <parslan@udesa.edu.ar>
2022-09-12 10:18:17 -0300

    agregue

---
+++ MyApp/database/config/config.js
@@ -0,0 +1,14 @@
+
+  module.exports = {
+    "development": {
+    "username": "root",
+    "password": "docker-db",
+    "database": "udesa", 
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+    }
+  }
+  
+  
+  
